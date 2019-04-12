0f539cbd71c5c38c09b42e3db8949e29985fe84f
Fufu Fang <fangfufu2003@gmail.com>
2019-04-12 13:52:11 +0100

    created the skeletal cache.c

--- src/cache.c
+++ src/cache.c
@@ -1,1 +1,43 @@
 #include "cache.h"
+
+off_t Cache_read(const char *filepath, off_t offset, size_t size)
+{
+}
+
+size_t Cache_write(const char *filepath, off_t offset, size_t size,
+                   const uint8_t *content)
+{
+}
+
+int Cache_create(const char *filepath, size_t size)
+{
+}
+
+
+Cache *Cache_open(const char *filepath)
+{
+}
+
+
+int Meta_write(Cache *cf)
+{
+}
+
+
+int Meta_read(Cache *cf)
+{
+}
+
+
+size_t Data_read(Cache *cf, off_t offset, size_t size)
+{
+}
+
+size_t Data_write(Cache *cf, off_t offset, size_t size,
+                  const uint8_t *content)
+{
+}
+
+int Data_create(Cache *cf, size_t size)
+{
+}
