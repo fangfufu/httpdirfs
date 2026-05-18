3bafaf06786264d1a51527ab723f1fad4dfa3b0f
Fufu Fang <fangfufu@fangfufu.co.uk>
2026-05-18 02:17:23 +0100

    chore: auto-format and stage files using pre-commit clang-format
    Update .pre-commit-config.yaml to replace the default pocc clang-format
    hook with a local system hook that automatically formats and stages
    modified C and header files. Use require_serial to avoid concurrent
    index lock issues.

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# This script formats the C source and header files using clang-format.
+# 'clang-format' is a required tool and must be available in your PATH.
 
 cd "${MESON_SOURCE_ROOT}"
 clang-format -i src/*.c src/*.h
