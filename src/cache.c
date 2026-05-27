f621da761117198b264a5fe211db71666d2bf115
Fufu Fang <fangfufu@fangfufu.co.uk>
2026-05-27 12:44:51 +0100

    refactor: add doxygen header and copyright notice
    Add file-level Doxygen annotations and GPLv3/OpenSSL copyright blocks
    to all C header and source files under the src/ directory. This ensures
    proper legal compliance and standardized documentation headers across the
    project.

--- src/cache.c
+++ src/cache.c
@@ -1,3 +1,31 @@
+/*
+ * HTTPDirFS - HTTP Directory Filesystem
+ *
+ * Copyright (C) 2020-2026 Fufu Fang <fangfufu2003@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the OpenSSL
+ * library.
+ */
+
+/**
+ * \file cache.c
+ * \brief Permanent cache system implementation
+ */
+
 #include "cache.h"
 
 #include "config.h"
